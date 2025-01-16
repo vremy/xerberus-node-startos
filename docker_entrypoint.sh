6cdb410ad07b9af0e8b2d3220b0dea680ebe0fc5
Vincent Remy <vremy@users.noreply.github.com>
2025-01-16 21:28:38 +0100

    Refactor docker_entrypoint.sh
    Add bootnodes configuration

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,9 +4,10 @@ printf "\n\n [i] Starting Xerberus Node ...\n\n"
 exec tini /usr/bin/xerberus-net \
   --base-path /data/nodes/testnet-node \
   --chain /data/chain-spec.json \
-  --rpc-external
-  --rpc-metods Unsafe \
+  --rpc-external \
+  --rpc-methods Unsafe \
   --rpc-cors all \
   --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
-  --prometheus-external
-  --no-private-ip
+  --prometheus-external \
+  --no-private-ip \
+  --bootnodes /dns/node.xerberus.io/tcp/30334/ws/p2p/12D3KooWCYKbsQw2r5575MA8YqMhn8AqhVuZkfobPMyoKzEP595t/p2p/12D3KooWCYKbsQw2r5575MA8YqMhn8AqhVuZkfobPMyoKzEP595t
