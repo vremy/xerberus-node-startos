9f9fa06de621713ef1074310df253ffeb0ec09ae
Vincent Remy <vremy@users.noreply.github.com>
2025-01-16 21:25:28 +0100

    Run Xerberus Node binary and specify node configuration

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-printf "\n\n [i] Starting Hello World ...\n\n"
-exec tini hello-world
+printf "\n\n [i] Starting Xerberus Node ...\n\n"
+exec tini /usr/bin/xerberus-net \
+  --base-path /data/nodes/testnet-node \
+  --chain /data/chain-spec.json \
+  --rpc-external
+  --rpc-metods Unsafe \
+  --rpc-cors all \
+  --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
+  --prometheus-external
+  --no-private-ip
